9e54b20427b096c930de75cbce193d342e29fddb
Henry Rice <code@henryrice.net>
2022-07-02 21:42:18 -0400

    inital

---
+++ dadbot-2.js
@@ -0,0 +1,12 @@
+/* 
+Trigger: im
+Description: Anytime a user uses starts a chat message with the keyword "i'm" NightBot wil unleash the ultimate dad joke, "Hi <What they wrote>>, I am NightBot"
+This version also enables you to target a specific user with an extra Zinger added to the end, i.e. "Hi <What they wrote>>, I am NightBot. And You Can Not Stop Me!"
+
+Custom Variables: 
+targetString = "FriendUN" <- Zinger Target, This is case sensitive
+zingString = "Message" <- Message to add after the "I am NightBot."
+ */
+
+
+$(eval tgtStg="FriendUN"; zStg=" And You Can Not Stop Me"; oStg="Hi, "; qStg=(decodeURIComponent("$(querystring $(query))") || decodeURIComponent("$(querystring $(user))")).substr(0,20); nbStg=", I am NightBot…Now learn to use proper punctuation!"; meStg = "$(user)"; try{tLStg=oStg+qStg+nbStg; if(meStg == tgtStg){tLStg=tLStg+zStg}; tLStg;}catch{`@admin DadBot Broke`})
