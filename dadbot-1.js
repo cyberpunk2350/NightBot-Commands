c302425011d1780d56c2dd212f7b29165b2968be
Henry Rice <code@henryrice.net>
2022-07-02 21:39:20 -0400

    changed substring to split

--- dadbot-1.js
+++ dadbot-1.js
@@ -1,12 +1,13 @@
 /* 
 Trigger: i'm
 Description: Anytime a user uses starts a chat message with the keyword "i'm" NightBot wil unleash the ultimate dad joke, "Hi <Wha they wrote>>, I am NightBot"
-This version also enables you to target a specific user with an extra Zinger added to the end, i.e. "Hi <Wha they wrote>>, I am NightBot. And You Can Not Stop Me!"
+This version also enables you to target a specific user with an extra Zinger added to the end, i.e. "Hi <What they wrote>>, I am NightBot. And You Can Not Stop Me!"
 
 Custom Variables: 
+admin
 targetString = "FriendUN" <- Zinger Target, This is case sensitive
 zingString = "Message" <- Message to add after the "I am NightBot."
  */
 
 
-$(eval targetString = "FriendUN"; zingString = " And You Can Not Stop Me!"; openString = "Hi "; quoteString = (decodeURIComponent("$(querystring $(query))") || decodeURIComponent("$(querystring $(user))")).substr(0,15); nbString = ", I am NightBot."; meString = "$(user)"; try{theLineString = openString + quoteString + nbString; if(meString == targetString){ theLineString = theLineString + zingString }; theLineString;}catch{if("$(provider)" == "discord"){`@NightBotAdmin DadBot Broke`}})
+$(eval targetString = "FriendUN"; zingString = " And You Can Not Stop Me!"; openString = "Hi, "; quoteString = (decodeURIComponent("$(querystring $(query))") || decodeURIComponent("$(querystring $(user))")).split(" "); nbString = ", I am NightBot."; meString = "$(user)"; try{theLineString = openString + quoteString[0] + nbString; if(meString == targetString){ theLineString = theLineString + zingString }; theLineString;}catch{`@admin DadBot Broke`})
