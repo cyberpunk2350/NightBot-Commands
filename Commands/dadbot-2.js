9a6a500c2d780cf6b23fcab4b1d26c7ef05ea636
Henry Rice <code@henryrice.net>
2022-07-02 22:16:54 -0400

    moved

--- Commands/dadbot-2.js
+++ Commands/dadbot-2.js
@@ -1,6 +1,7 @@
 /* 
 Trigger: im
 Description: Anytime a user uses starts a chat message with the keyword "i'm" NightBot wil unleash the ultimate dad joke, "Hi <What they wrote>>, I am NightBot"
+This version will also give the user a lashing for not using proper punctuation.
 This version also enables you to target a specific user with an extra Zinger added to the end, i.e. "Hi <What they wrote>>, I am NightBot. And You Can Not Stop Me!"
 
 Custom Variables: 
